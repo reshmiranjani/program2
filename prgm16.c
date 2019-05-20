055a75d0c5ecebeeb72f0081c78489d25cadd7a4
reshmiranjani <50790684+reshmiranjani@users.noreply.github.com>
2019-05-20 21:40:49 +0530

    Create prgm16.c

---
+++ prgm16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main()
+{
+    int a,b,i,count;
+    scanf("%d %d", &a, &b);
+    while (a < b)
+    {
+        count = 0;
+
+        for(i = 2; i <= a/2; ++i)
+        {
+            if(a % i == 0)
+            {
+                count= 1;
+                break;
+            }
+        }
+
+        if (count == 0)
+            printf("%d ",a);
+
+        ++a;
+    }
+
+    return 0;
+
+}
