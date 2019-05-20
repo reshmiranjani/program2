1b645ac9b28dd1bcfd8543b25c3fdbae6ba75801
reshmiranjani <50790684+reshmiranjani@users.noreply.github.com>
2019-05-20 18:59:08 +0530

    Create prgm15.c

---
+++ prgm15.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+void  main()
+{
+  int a,b,i;
+  scanf("%d%d",&a,&b);
+  if(a<=10000&&b<=10000)
+  {
+  for(i=a+1;i<b;i++)
+  {
+      if(i%2==0)
+      {
+          printf("%d ", i);
+      }
+  }
+  }
+  else
+  {
+      printf("invalid");
+  }
+  }
+   
